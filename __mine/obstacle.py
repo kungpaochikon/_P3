d54b3927a28fe897a1d722e83a04072d8f8b9d33
gary <flemingg@mail.gvsu.edu>
2018-04-09 22:03:35 -0400

    Base Game Done

--- __mine/obstacle.py
+++ __mine/obstacle.py
@@ -3,19 +3,31 @@ import pygame
 class Obstacle(pygame.sprite.Sprite):
     def __init__(self,panel,x,midY,top):
         pygame.sprite.Sprite.__init__(self)
-        self.screen = panel
-        self.x = x
-        self.midY = midY
-        self.top = top
         self.image = pygame.image.load("obs.png")
         self.rect = self.image.get_rect()
         self.w = self.rect.width
+        self.h = self.rect.height
+        self.screen = panel
+        self.x = x
+        diff = 64
+        self.midY = midY
+        self.y = midY + diff
+        self.top = top
+        if(top):
+            self.y -= 512
+
         
         
-        self.paint()
+        self.updateDisplay()
 
     def updateDisplay(self):
         self.screen.blit(self.image,(self.x,self.y))
         self.rect.x,self.rect.y = self.x,self.y
         
-    
+    def step(self):
+        self.x -= 3
+
+        self.updateDisplay()
+
+    def setMidY(self):
+        self
